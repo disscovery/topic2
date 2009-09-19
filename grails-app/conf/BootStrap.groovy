c276934a000f18060afb98dd10c0ea62073a6ad6
Nat <nat.weerawan@gmail.com>
2009-09-19 16:06:43 +0700

    can add to domain class(News table) from file

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,7 @@
 class BootStrap {
 
      def init = { servletContext ->
+		new News(news:'myString',lexnews:'longlexnews',corrected:null).save()
      }
      def destroy = {
      }
--- grails-app/controllers/JavaController.groovy
+++ grails-app/controllers/JavaController.groovy
@@ -5,10 +5,8 @@ import java.*
 import groovy.util.*
 /*/	System.setProperty( "file.encoding", "UTF-8" );*/
 class JavaController {
-	
-   // HelloWorld tt = new HelloWorld();
+ 
 	def myTypeList,myIndexList=[]
-	//cc.Test();
     def index = { 
 	 System.out.println("Encoding: " + System.getProperty("file.encoding"));
      System.out.println("Charset: " + java.nio.charset.Charset.defaultCharset());
@@ -16,33 +14,36 @@ class JavaController {
 	def lexdict=new File("lexitron.txt")
 	LongLexTo tokenizer=new LongLexTo("SELFED DICT")
 	lexdict.eachLine{ tokenizer.addDict(it.trim()) }
-	println "lexsize="+lexdict.size()
- //	def news=new File("news.txt").getText("MacRoman")
 	def news=new File("news.txt")
- 
 	news.eachLine{
- 
 	tokenizer.wordInstance(it.trim())
 		myTypeList = tokenizer.getTypeList()
 		myIndexList=tokenizer.getIndexList()
-		myString = [it]
-	}
-	println System.getProperty("user.dir");
-	def indexer=0
-	myIndexList.eachWithIndex{ num, idx ->
-		def type=myTypeList[idx]
-		render  myString[0][indexer..num-1] + "|" 
-		indexer=num
+		myString = it
+
+
+		def indexer=0
+		def longlexnews=""
+		myIndexList.eachWithIndex{ num, idx ->
+			//def type=myTypeList[idx]
+			longlexnews+= myString[indexer..num-1] + "|" 
+			indexer=num
+		}
+	new News(news:myString,lexnews:longlexnews,corrected:false).save()
+/*		println result*/
+		render longlexnews + "nat2"
+		
+		
+				
 	}
 	
+	println System.getProperty("user.dir");
+
 	println myIndexList
 	println myTypeList
-//	println 
-	render "TEST"
-//	Charset guessedCharset = CharsetToolkit.guessEncoding("ทดสอบ", 4096);
+ 
 
 	 System.out.println("Encoding: " + System.getProperty("file.encoding"));
     System.out.println("Charset: " + java.nio.charset.Charset.defaultCharset());
-/*		println "lexsize="+lexdict.size()*/
 	 } 
 }
